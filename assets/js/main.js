ced12e2da15ca2f38b3436492280d4b9e92e06fa
oscar <oscarelia16@gmail.com>
2016-06-26 01:54:00 -0600

    Agregando las katakanas

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,8 @@
 
 		$('.alert.alert-success').hide();
 		$('.alert.alert-danger').hide();
-		showRandom();
+		showRandomHiragana();
+		showRandomKatakana();
 
 	});
 
@@ -25,11 +26,16 @@
 	var hiraganaLetter = document.getElementById("hiragana-letter");
 	var katakanaLetter = document.getElementById("katakana-letter");
 
-		
 
-	function showRandom(){
+
+	function showRandomHiragana(){
 		var letter= hiragana[Math.floor(Math.random() * hiragana.length)];
 		hiraganaLetter.innerHTML = letter;
+	}
+
+	function showRandomKatakana(){
+		var letter= katakana[Math.floor(Math.random() * katakana.length)];
+		katakanaLetter.innerHTML = letter;
 	}
 
 	function showMessage(){
@@ -53,8 +59,8 @@
 
 
 		
-	function validateLetter(){
-		var letterToCheck = document.getElementById("letter-to-validate").value;
+	function validateHiragana(){
+		var letterToCheck = document.getElementById("letter-to-validate-hiragana").value;
 		letterToCheck = letterToCheck.toLowerCase();
 
 		switch (letterToCheck)
@@ -210,7 +216,7 @@
 
 		if(hiraganaLetter.innerHTML==letterToCheck){
 			hideError();
-			showRandom();
+			showRandomHiragana();
 			showMessage();
 			//setTimeout(hideMessage, 1000);
 			
@@ -222,4 +228,175 @@
 
 
 		
+	}
+
+
+
+
+
+
+	function validateKatana(){
+		var letterToCheck = document.getElementById("letter-to-validate-katakana").value;
+		letterToCheck = letterToCheck.toLowerCase();
+
+		switch (letterToCheck)
+		{
+			case "a":
+				letterToCheck=katakana[0];
+				break;
+			case "i":
+				letterToCheck=katakana[1];
+				break;
+			case "u":
+				letterToCheck=katakana[2];
+				break;
+			case "e":
+				letterToCheck=katakana[3];
+				break;
+			case "o":
+				letterToCheck=katakana[4];
+				break;
+			case "ka":
+				letterToCheck=katakana[5];
+				break;
+			case "ki":
+				letterToCheck=katakana[6];
+				break;
+			case "ku":
+				letterToCheck=katakana[7];
+				break;
+			case "ke":
+				letterToCheck=katakana[8];
+				break;
+			case "ko":
+				letterToCheck=katakana[9];
+				break;
+			case "sa":
+				letterToCheck=katakana[10];
+				break;
+			case "si":
+				letterToCheck=katakana[11];
+				break;
+			case "su":
+				letterToCheck=katakana[12];
+				break;
+			case "se":
+				letterToCheck=katakana[13];
+				break;
+			case "so":
+				letterToCheck=katakana[14];
+				break;
+			case "ta":
+				letterToCheck=katakana[15];
+				break;
+			case "ti":
+				letterToCheck=katakana[16];
+				break;
+			case "tu":
+				letterToCheck=katakana[17];
+				break;
+			case "te":
+				letterToCheck=katakana[18];
+				break;
+			case "to":
+				letterToCheck=katakana[19];
+				break;
+			case "na":
+				letterToCheck=katakana[20];
+				break;
+			case "ni":
+				letterToCheck=katakana[21];
+				break;
+			case "nu":
+				letterToCheck=katakana[22];
+				break;
+			case "ne":
+				letterToCheck=katakana[23];
+				break;
+			case "no":
+				letterToCheck=katakana[24];
+				break;
+			case "ha":
+				letterToCheck=katakana[25];
+				break;
+			case "hi":
+				letterToCheck=katakana[26];
+				break;
+			case "hu":
+				letterToCheck=katakana[27];
+				break;
+			case "he":
+				letterToCheck=katakana[28];
+				break;
+			case "ho":
+				letterToCheck=katakana[29];
+				break;
+			case "ma":
+				letterToCheck=katakana[30];
+				break;
+			case "mi":
+				letterToCheck=katakana[31];
+				break;
+			case "mu":
+				letterToCheck=katakana[32];
+				break;
+			case "me":
+				letterToCheck=katakana[33];
+				break;
+			case "mo":
+				letterToCheck=katakana[34];
+				break;
+			case "ya":
+				letterToCheck=katakana[35];
+				break;
+			case "yu":
+				letterToCheck=katakana[36];
+				break;
+			case "yo":
+				letterToCheck=katakana[37];
+				break;
+			case "ra":
+				letterToCheck=katakana[38];
+				break;
+			case "ri":
+				letterToCheck=katakana[39];
+				break;
+			case "ru":
+				letterToCheck=katakana[40];
+				break;
+			case "re":
+				letterToCheck=katakana[41];
+				break;
+			case "ro":
+				letterToCheck=katakana[42];
+				break;
+			case "wa":
+				letterToCheck=katakana[43];
+				break;
+			case "wo":
+				letterToCheck=katakana[44];
+				break;
+			case "n":
+				letterToCheck=katakana[45];
+				break;
+
+			default:
+				console.log("no esta agregado aún!!")
+
+		}
+
+		if(katakanaLetter.innerHTML==letterToCheck){
+			hideError();
+			showRandomKatakana();
+			showMessage();
+			//setTimeout(hideMessage, 1000);
+
+		}
+		else {
+			hideMessage();
+			showError();
+		}
+
+
+
 	}
